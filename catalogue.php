e2370b39dbfe0465bce7ab589d65d1908f030b6d
Hakim <hakim@antilop.fr>
2015-11-28 16:05:30 +0100

    Mise à jour

--- catalogue.php
+++ catalogue.php
@@ -1,7 +1,5 @@
 <?php
 
-$url = 'https://api.cdiscount.com/OpenApi/json/Search';
-
 $search_request = new stdClass();
 $search_request->Keyword = 'Ps3';
 $search_request->SortBy = 'minprice';
@@ -18,7 +16,7 @@ $search_request->Filters->Brands = ['sony'];
 $search_request->Filters->Condition = true;
 
 $fields = array(
-	'ApiKey' => urlencode('75243e65-dfd6-4058-91b0-9e2a89573eb8'),
+	'ApiKey' => '75243e65-dfd6-4058-91b0-9e2a89573eb8',
 	'SearchRequest' => $search_request
 );
 
@@ -26,6 +24,7 @@ $fields = array(
 $data_string = json_encode($fields);                                                                                   
 echo '<pre>';
 var_dump($data_string);
+$url = 'https://api.cdiscount.com/OpenApi/json/Search';
 $ch = curl_init($url);                                                                      
 curl_setopt($ch, CURLOPT_POST      ,1);                                                                     
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
